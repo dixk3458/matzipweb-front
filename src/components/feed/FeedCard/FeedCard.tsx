3fa0b16356e0bf9b61faba48f43214b8f873b640
Jaewoong <dixk3458@naver.com>
2024-08-13 17:06:58 +0900

    FeedHome 스타일링 수정

--- src/components/feed/FeedCard/FeedCard.tsx
+++ src/components/feed/FeedCard/FeedCard.tsx
@@ -10,24 +10,22 @@ function FeedCard({ feed }: FeedCardProps) {
   const { title, description, images, score, color } = feed;
   return (
     <div className={styles.container}>
-      <div className={styles.imageWrapper}>
-        {images.length > 0 ? (
-          <img
-            src={images[0].uri}
-            alt={`${title}-feed`}
-            className={styles.image}
-          />
-        ) : (
-          <div className={styles.noImage}>No Image Available</div>
-        )}
-      </div>
-      <div className={styles.infoTextContainer}>
+      {images.length > 0 ? (
+        <img
+          src={images[0].uri}
+          alt={`${title}-feed`}
+          className={styles.image}
+        />
+      ) : (
+        <div className={styles.noImage}>No Image Available</div>
+      )}
+      <div className={styles.content}>
         <h3 className={styles.titleText}>{title}</h3>
-        <div className={styles.metaContainer}>
-          <p className={styles.scoreText}>{`Score ${score}`}</p>
-          <MarkerIcon size={24} color={color} />
-        </div>
         <p className={styles.descriptionText}>{description}</p>
+        <div className={styles.meta}>
+          <span className={styles.score}>{`Score ${score}`}</span>
+          <MarkerIcon size={20} color={color} />
+        </div>
       </div>
     </div>
   );
--- src/pages/feed/FeedHome/FeedHomePage.tsx
+++ src/pages/feed/FeedHome/FeedHomePage.tsx
@@ -1,15 +1,14 @@
 import useAuth from '../../../hooks/queries/useAuth';
 import useGetAllPostsByUserId from '../../../hooks/queries/useGetAllPostsByUserId';
-
 import styles from './FeedHomePage.module.css';
 import FeedCard from '../../../components/feed/FeedCard/FeedCard';
 import { Link } from 'react-router-dom';
 
 function FeedHomePage() {
   const { getProfileQuery } = useAuth();
-  const { id: userId } = getProfileQuery.data! || {};
+  const { id: userId } = getProfileQuery.data || {};
 
-  const { data: feeds, isLoading, error } = useGetAllPostsByUserId(userId);
+  const { data: feeds, isLoading, error } = useGetAllPostsByUserId(userId!);
 
   if (isLoading) {
     return <p>로딩중...</p>;
@@ -27,9 +26,13 @@ function FeedHomePage() {
     <section className={styles.container}>
       <ul className={styles.feedContainer}>
         {feeds &&
-          feeds.map((feed, index) => (
-            <li key={feed.id}>
-              <Link to={`/feed/${feed.id}`} state={{ feedId: feed.id }}>
+          feeds.map(feed => (
+            <li key={feed.id} className={styles.item}>
+              <Link
+                to={`/feed/${feed.id}`}
+                state={{ feedId: feed.id }}
+                className={styles.link}
+              >
                 <FeedCard feed={feed} />
               </Link>
             </li>
