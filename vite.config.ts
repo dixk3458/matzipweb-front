cac4628f4cb0ba160e10684b9008f1b9fb0b06b2
jaewoong <dixk3458@naver.com>
2025-05-31 20:45:58 +0800

    SEO 작업

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import prerender from '@prerenderer/rollup-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      routes: ['/', '/signin', '/signup'],
+      renderer: '@prerenderer/renderer-puppeteer',
+      server: {
+        port: 3000,
+        host: 'localhost',
+      },
+      rendererOptions: {
+        maxConcurrentRoutes: 1,
+        renderAfterTim: 500,
+      },
+      postProcess(renderedRoute) {
+        renderedRoute.html = renderedRoute.html
+          .replace(/http:/i, 'https:')
+          .replace(
+            /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
+            'https://dm3lc0kermpza.cloudfront.net/'
+          );
+      },
+    }),
+  ],
 });
