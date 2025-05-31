282b9448e58346fb18809081f1e776b887236501
jaewoong <dixk3458@naver.com>
2025-05-31 21:23:35 +0900

    migrate to vite

--- src/apis/map.ts
+++ src/apis/map.ts
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 async function getAddress(lat: number, lng: number) {
   const { data } = await axios.get(
-    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&result_type=street_address|route|political&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&language=ko`
+    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&result_type=street_address|route|political&key=${
+      import.meta.env.VITE_GOOGLE_MAPS_API_KEY
+    }&language=ko`
   );
 
   return data;
--- src/pages/map/MapHome/MapHomePage.tsx
+++ src/pages/map/MapHome/MapHomePage.tsx
@@ -44,7 +44,7 @@ function MapHomePage() {
       : markers.filter(marker => currentMarkerFilter.includes(marker.color));
 
   const { isLoaded, loadError } = useJsApiLoader({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? '',
+    googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAPS_API_KEY ?? '',
   });
 
   const [map, setMap] = useState<google.maps.Map | null>(null);
